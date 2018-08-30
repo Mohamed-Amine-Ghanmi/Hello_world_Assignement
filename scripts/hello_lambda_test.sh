8f826ba33f7625d20c16f37162c065c0eedb741a
Mohamed Amine Ghanmi <ghanmi.med.amine90@gmail.com>
2018-08-30 10:59:01 -0700

    Update hello_lambda_test.sh

--- scripts/hello_lambda_test.sh
+++ scripts/hello_lambda_test.sh
@@ -5,5 +5,5 @@ version=$2
 old_pwd=`pwd`
 
 echo “Testing lambda”
-/var/lib/jenkins/.local/bin/python-lambda-local -f handler -t 5 $old_pwd/hello_lambda.py $old_pwd/scripts/event.json
-/var/lib/jenkins/.local/bin/python-lambda-local -f post_handler -t 5 $old_pwd/hello_lambda.py $old_pwd/scripts/event.json
+~/.local/bin/python-lambda-local -f handler -t 5 $old_pwd/hello_lambda.py $old_pwd/scripts/event.json
+~/.local/bin/python-lambda-local -f post_handler -t 5 $old_pwd/hello_lambda.py $old_pwd/scripts/event.json
